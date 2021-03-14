c234b88e24f8a623ff1db2670da34b838860cccd
romaindupont <rd.romaindupont@free.fr>
2021-03-14 12:08:46 +0100

    contact form nom, mail

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,10 +3,14 @@ import classNames from 'classnames';
 import './style.scss';
 
 const Contact = () => {
-  const [onFocus, setOnFocus] = useState(false);
+  const [onFocusEmail, setOnFocusEmail] = useState(false);
+  const [onFocusName, setOnFocusName] = useState(false);
   const handleBlur = () => {
-    setOnFocus(!onFocus)
-    //console.log(onFocus)
+    setOnFocusEmail(true)
+   //console.log(onFocus)
+  }
+  const handleClickName = () => {
+      setOnFocusName(true)
   }
   let url ='';
   return (
@@ -14,15 +18,18 @@ const Contact = () => {
         <h2 className="contact-title">Contact</h2>
         <form className="form" method="post">
             <div className="form-email">
-                <label className={classNames("form-email-label", {'form-email-label--blur':onFocus})}></label>
-                <input onFocus={handleBlur} onBlur={handleBlur} className="form-email-input" type="email" required></input>
+                <label className={classNames("form-email-label", {'form-email-label--blur':onFocusEmail})}></label>
+                <input onClick={handleBlur} className="form-email-input" type="email" required></input>
             </div>
-            <label  className="form-label">Votre nom
-                <input className="form-input" type="text"></input>
-            </label>
-            <label className="form-label">Votre message
-                <textarea rows="5" cols="33">Votre message ici...</textarea>
-            </label>
+            <div className="form-name">
+                <label className={classNames("form-name-label", {'form-name-label--blur':onFocusName})}></label>
+                    <input onClick={handleClickName} className="form-name-input" type="text"></input>
+            </div>
+            <div className="form-textarea">
+                <label className="form-textarea-label"></label>
+                    <textarea rows="5" cols="33" className="form-textarea-input">Votre message ici...</textarea>
+            </div>
+            
             <button type="submit">Send It</button>
         </form>
         <div className="contact-logo">
