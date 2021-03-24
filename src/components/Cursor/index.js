077bed52e2d6085425b73e10d3b4adb544bec79a
romaindupont <rd.romaindupont@free.fr>
2021-03-24 10:58:55 +0000

    cursor custom

---
+++ src/components/Cursor/index.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+import classNames from 'classnames';
+import './style.scss';
+
+
+
+const Cursor = ()=> {
+   const [position, setPosition] = useState({
+      x: 0,
+      y: 0
+  })
+  const [linkHovered, setLinkHovered] = useState(false);
+  const [imgHovered, setImgHovered] = useState(false);
+useEffect(() => {
+      addEventListeners();
+      handleLinkHoverEvents();
+      return () => removeEventListeners();
+   }, []);
+   const handleLinkHoverEvents = () => {
+       document.querySelectorAll("a").forEach(el => {
+           el.addEventListener("mouseover", () => setLinkHovered(true));
+           el.addEventListener("mouseout", () => setLinkHovered(false));
+       });
+              document.querySelectorAll("img").forEach(el => {
+           el.addEventListener("mouseover", () => setImgHovered(true));
+           el.addEventListener("mouseout", () => setImgHovered(false));
+       });
+   };
+   const addEventListeners = () => {
+       document.addEventListener("mousemove", onMouseMove);
+   };
+
+   const removeEventListeners = () => {
+       document.removeEventListener("mousemove", onMouseMove);
+   };
+
+   const onMouseMove = (e) => {
+       setPosition({x: e.clientX, y: e.clientY});
+   };            
+
+  return (
+    <div className={classNames("cursor", {"cursor--linkHover":linkHovered,"cursor--imgHover":imgHovered })} style={{
+      left: `${position.x}px`,
+      top: `${position.y}px`
+      }}
+    />
+  
+  )}
+export default Cursor;
